8a7a92162baa5bd0281100ce075a5f6fdeb4968d
fbansept <bansept.franck@gmail.com>
2022-10-26 12:01:18 +0200

    ajout 100 balles

--- src/main/java/CasseBrique.java
+++ src/main/java/CasseBrique.java
@@ -36,8 +36,15 @@ public class CasseBrique extends Canvas {
 
     public void demarrer() throws InterruptedException {
 
-        Balle balle1 = new Balle(200, 200,Color.RED,50);
-        Balle balle2 = new Balle(100, 300,Color.BLUE,40);
+        Balle[] tableauBalle = new Balle[100];
+
+        int positionDepart = 0;
+
+        for (int i = 0; i < tableauBalle.length; i ++){
+            tableauBalle[i] = new Balle(positionDepart,positionDepart,Color.RED,20);
+            positionDepart += 3;
+        }
+
 
         while(true) {
 
@@ -46,13 +53,13 @@ public class CasseBrique extends Canvas {
             dessin.setColor(Color.WHITE);
             dessin.fillRect(0,0, largeur, hauteur);
 
-            balle1.mouvement();
-            balle1.collision(largeur,hauteur);
-            balle1.dessiner(dessin);
+            for(Balle balle : tableauBalle) {
+                balle.mouvement();
+                balle.collision(largeur,hauteur);
+                balle.dessiner(dessin);
+            }
 
-            balle2.mouvement();
-            balle2.collision(largeur,hauteur);
-            balle2.dessiner(dessin);
+
 
             //-----------------------------
             dessin.dispose();
